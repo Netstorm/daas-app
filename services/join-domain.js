75be196f42c29257870347db65c99054f4325edf
Administrator@MHS.LOCAL <Administrator@MHS.LOCAL>
2018-12-14 12:25:15 +1100

    removed drive mapping

--- services/join-domain.js
+++ services/join-domain.js
@@ -20,8 +20,6 @@ module.exports = {
 	$interfaceIndex = (Get-NetAdapter | Where-object {$_.Name -like "*Ethernet*" } | Select-Object -ExpandProperty InterfaceIndex)
   Log-Write "INFO InterfaceIndex=$interfaceIndex Setting DNS"
   Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses "${process.env.DC_PRIMARY_DNS}","${process.env.DC_SECONDARY_DNS}"
-  Log-Write "INFO Mapping network drive"
-  New-SmbMapping -LocalPath Z: -RemotePath \\17cc2a48158-kfw39.ap-southeast-2.nas.aliyuncs.com\myshare -Persistent:$True
   $domain = "${process.env.DOMAIN}"
 	$adminUser = "${process.env.NETBIOS}\\Administrator"
 	$adminPass = "${process.env.AD_ADMIN_PASS}" | ConvertTo-SecureString -AsPlainText -Force
