2711165b91d914b6589ceaebcd9db7f634794342
Administrator@MHSVDI.WAN <Administrator@MHSVDI.WAN>
2018-12-31 14:39:57 +1100

    script changes

--- services/join-domain.js
+++ services/join-domain.js
@@ -17,33 +17,6 @@ module.exports = {
     Add-content $Logfile -value "$(Get-TimeStamp)$logString"
   }
   Log-Write "INFO InstanceId=$instanceId"
-  Try {
-    $TaskName = "Notify Idle Instance"
-    $service = New-Object -ComObject("Schedule.Service")
-    $service.Connect()
-    $rootFolder = $service.GetFolder("")
-    $taskdef = $service.NewTask(0)
-    $taskdef.Principal.RunLevel = 1
-    $sets = $taskdef.Settings
-    $sets.AllowDemandStart = $false
-    $sets.Compatibility = 2
-    $sets.Enabled = $true
-    $sets.RunOnlyIfIdle = $true
-    $sets.IdleSettings.IdleDuration = "PT10M"
-    $sets.IdleSettings.WaitTimeout = "PT0M"
-    $sets.IdleSettings.StopOnIdleEnd = $true
-    $sets.IdleSettings.RestartOnIdle = $true
-    $trg = $taskdef.Triggers.Create(6)
-    $trg.Enabled = $true
-    $act = $taskdef.Actions.Create(0)
-    $act.Path = "powershell.exe"
-    $act.Arguments = "-ExecutionPolicy Bypass -File C:\\Users\\MHUSER\\mydesktop\\NotifyIdleInstance.ps1"
-    $rootFolder.RegisterTaskDefinition($TaskName, $taskdef, 6, 'Users', $null, 4)
-    Log-Write "Scheduled task created"
-  }
-  Catch {
-    Log-Write "ERROR $_.Exception"
-  }
 	$interfaceIndex = (Get-NetAdapter | Where-object {$_.Name -like "*Ethernet*" } | Select-Object -ExpandProperty InterfaceIndex)
   Log-Write "INFO InterfaceIndex=$interfaceIndex Setting DNS"
   Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses "${process.env.DC_PRIMARY_DNS}","${process.env.DC_SECONDARY_DNS}"
@@ -55,6 +28,33 @@ module.exports = {
       Log-Write "INFO Adding computer to domain $domain"
 			Add-Computer -DomainName $domain -OUPath "${process.env.OU_PATH}" -Options AccountCreate -Credential $cred -Force -Restart -ErrorAction 'Stop'
   	}
+    Catch {
+      Log-Write "ERROR $_.Exception"
+    }
+    Try {
+      $TaskName = "Notify Idle Instance"
+      $service = New-Object -ComObject("Schedule.Service")
+      $service.Connect()
+      $rootFolder = $service.GetFolder("")
+      $taskdef = $service.NewTask(0)
+      $taskdef.Principal.RunLevel = 1
+      $sets = $taskdef.Settings
+      $sets.AllowDemandStart = $false
+      $sets.Compatibility = 1
+      $sets.Enabled = $true
+      $sets.RunOnlyIfIdle = $true
+      $sets.IdleSettings.IdleDuration = "PT10M"
+      $sets.IdleSettings.WaitTimeout = "PT0M"
+      $sets.IdleSettings.StopOnIdleEnd = $true
+      $sets.IdleSettings.RestartOnIdle = $true
+      $trg = $taskdef.Triggers.Create(6)
+      $trg.Enabled = $true
+      $act = $taskdef.Actions.Create(0)
+      $act.Path = "powershell.exe"
+      $act.Arguments = "-ExecutionPolicy Bypass -File C:\\Users\\MHUSER\\mydesktop\\NotifyIdleInstance.ps1"
+      $rootFolder.RegisterTaskDefinition($TaskName, $taskdef, 6, 'Users', $null, 4)
+      Log-Write "Scheduled task created"
+    }
     Catch {
       Log-Write "ERROR $_.Exception"
     }
