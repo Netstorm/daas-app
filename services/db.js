6b4342c4f19856ab51dbd0b8f6f8795a448ee76e
Administrator@MHSVDI.WAN <Administrator@MHSVDI.WAN>
2019-01-15 14:10:44 +1100

    handle db disconnections

--- services/db.js
+++ services/db.js
@@ -1,13 +1,35 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var DB_CONFIG = {
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_DBNAME
-});
+}
 
-connection.connect();
+var connection;
+
+function handleDisconnect() {
+	connection = mysql.createConnection(DB_CONFIG);	// Recreate the connection, since the old one cannot be reused.
+
+	connection.connect(function (err) {              // The server is either down
+		if (err) {                                     // or restarting (takes a while sometimes).
+			console.log('error when connecting to db:', err);
+			setTimeout(handleDisconnect, 3000); // We introduce a delay before attempting to reconnect,
+		}                                     // to avoid a hot loop, and to allow our node script to
+	});                                     // process asynchronous requests in the meantime.
+	// If you're also serving http, display a 503 error.
+	connection.on('error', function (err) {
+		console.log('db error', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+			handleDisconnect();                         // lost due to either server restart, or a
+		} else {                                      // connnection idle timeout (the wait_timeout
+			throw err;                                  // server variable configures this)
+		}
+	});
+}
+
+handleDisconnect();
 
 function getUser(username) {
 	return new Promise((resolve, reject) => {
